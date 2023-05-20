153e2b93eb92ce29e278810f5e6ed657dcf49cdc
Keith Moss <keithamoss@gmail.com>
2023-05-20 12:40:34 +0800

    CI

--- frontend/build.sh
+++ frontend/build.sh
@@ -8,8 +8,18 @@ if [ ! -d "node_modules" ]; then
     npm install
 fi
 
-npm run lint
+# npm run lint
 
-sudo npm run build
+npm run build
+
+# pwd
 
 cd ./build && tar czvf /build/frontend.tgz .
+
+# cd ./build
+
+# ls -l
+
+# tar czvf /build/frontend.tgz .
+
+ls -l /build
