6137781b1428876feec8c83bfb9ee59189f35d15
Keith Moss <keithamoss@gmail.com>
2023-05-20 13:13:22 +0800

    CI should work now

--- frontend/build.sh
+++ frontend/build.sh
@@ -5,21 +5,13 @@ mkdir -p ./build
 
 # For GitHub actions
 if [ ! -d "node_modules" ]; then
-    npm install
+    yarn install
 fi
 
-# npm run lint
-
-npm run build
-
-# pwd
+# For some reason npm throws:
+# EACCES: permission denied, mkdir '/app/build/app-icon'
+# So whatever, let's just use yarn to build for now
+# and see if switching to vite fixes this
+yarn run build
 
 cd ./build && tar czvf /build/frontend.tgz .
-
-# cd ./build
-
-# ls -l
-
-# tar czvf /build/frontend.tgz .
-
-ls -l /build
