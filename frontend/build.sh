0557f0365bd032f6973683622428c30e904e38f8
Keith Moss <keithamoss@gmail.com>
2023-05-20 12:30:50 +0800

    More CI deugging

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-rm -rf ./build
-mkdir -p ./build
+# rm -rf ./build
+# mkdir -p ./build
 
-# For GitHub actions
-if [ ! -d "node_modules" ]; then
-    npm install
-fi
+# # For GitHub actions
+# if [ ! -d "node_modules" ]; then
+#     npm install
+# fi
 
-npm run lint
+# npm run lint
 
-chown -R node:node /app/build/app-icon
 npm run build
 
+# pwd
+
 cd ./build && tar czvf /build/frontend.tgz .
+
+# cd ./build
+
+# ls -l
+
+# tar czvf /build/frontend.tgz .
+
+ls -l /build
