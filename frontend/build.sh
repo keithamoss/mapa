74dbaca62111ea742a2ad4265d9e188f5b74adad
Keith Moss <keith.moss@dpc.wa.gov.au>
2023-09-25 16:39:47 +0800

    Avoid JavaScript heap out of memory errors from Vite

--- frontend/build.sh
+++ frontend/build.sh
@@ -3,6 +3,9 @@
 rm -rf ./build
 mkdir -p ./build
 
+# Avoid JavaScript heap out of memory errors from Vite
+# https://github.com/vitejs/vite/issues/2433
+export NODE_OPTIONS=--max-old-space-size=8192
 yarn run build
 
 cd ./build && tar czvf /build/frontend.tgz .
