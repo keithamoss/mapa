d01a9e60d9f031bdc66576961944d0137a50402a
Keith Moss <keith.moss@dpc.wa.gov.au>
2023-09-26 15:28:42 +0800

    Adding the migration file helps...

--- django/mapa/migration/migrate.py
+++ django/mapa/migration/migrate.py
@@ -1,88 +1,117 @@
 import csv
 import json
-import random
 
 import django
+from django.forms.models import model_to_dict
 
 django.setup()
 
+IMPORT_JOB_NAME = "20230926 Foraging Map Migration"
 JSON_DATA_FILE_PATH = "./content.json"
-FORAGING_FOLDER_ID = 2
-FORAGING_MAP_ID = 22
-FORAGING_SCHEMA_ID = 64
-
-
-def translate_folder_to_map(folderId, folders):
-    folder = [f for f in folders if f["id"] == folderId][0]
-
-    if folder is not None:
-        if folder["name"] == "Foraging":
-            return FORAGING_MAP_ID
-        elif folder["name"] == "Interesting Perth" or folder["name"] == "Interesting Sydney":
-            return -1
-
-    raise Exception("Unknown folder")
-
+CSV_DATA_FILE_PATH = "./MIGRATED_map_marker_contents_30Aug2023 - map_marker_contents_30Aug2023.csv"
+# Keith's Local Dev
+# FORAGING_MAP_ID = 26
+# FORAGING_SCHEMA_ID = 67
+# Production
+FORAGING_MAP_ID = 7
+FORAGING_SCHEMA_ID = 6
 
 def migrate():
     from mapa.app.enums import GeomType
-    from mapa.app.models import Features
+    from mapa.app.models import Features, FeatureSchemas
     from mapa.app.serializers import FeatureSerializer
 
+    # Check for (and manually purge) any features already imported by this import job
+    feats = Features.objects.filter(import_job=IMPORT_JOB_NAME)
+    if len(feats) > 0:
+      # feats.delete()
+      raise Exception("Found some features already imported")
+
+    # Grab symbols from our schema
+    foragingSchema = model_to_dict(FeatureSchemas.objects.get(id=FORAGING_SCHEMA_ID))
+    symbols = {}
+    for symbol in foragingSchema["symbology"]["symbols"]:
+      symbol_name = symbol["props"]["name"].lower()
+      if symbol_name in symbols:
+          raise Exception(symbol_name)
+      symbols[symbol_name] = symbol
+
+    # Grab the original Map Marker JSON file
     with open(JSON_DATA_FILE_PATH, "r") as f:
-        Features.objects.filter(map_id=FORAGING_MAP_ID).delete()
+        json_pois = {}
+        for feature in json.load(f)["data_content"]["poi"]:
+            if feature["id"] in json_pois:
+                raise Exception(feature["id"])
+            json_pois[str(feature["id"])] = feature
+    
+    # Grab the migrated Map Marker CSV file that maps across to Mapa symbols
+    with open(CSV_DATA_FILE_PATH, "r") as f:
+        csv_file = csv.DictReader(f)
 
-        data = json.load(f)["data_content"]
-        folders = data["folder"]
+        csv_pois = {}
+        for feature in csv_file:
+            if feature["id"] in csv_pois:
+                raise Exception(feature["id"])
+            csv_pois[str(feature["id"])] = feature
 
-        csv_out = []
+    for featureId, feature in csv_pois.items():
+        print(featureId)
+        if featureId not in json_pois:
+            raise Exception(featureId)
+        
+        json_feature = json_pois[featureId]
 
-        for feature in data["poi"]:
-            # print(feature)
+        # Make sure the matching symbol exists
+        symbol_name = feature["icon_field"].lower()
+        if symbol_name not in symbols:
+          raise Exception(symbol_name)
+        symbol = symbols[symbol_name]
 
-            if feature["folder_id"] != FORAGING_FOLDER_ID:
-                continue
+        # Build the data object
+        data = []
+        if feature["attr_has_lots"] == "1":
+          data.append({
+            "value": True,
+            "schema_field_id": 1
+          })
+        if feature["attrs_is_high"] == "1":
+          data.append({
+            "value": True,
+            "schema_field_id": 2
+          })
+        if feature["attrs_uncertain"] == "1":
+          data.append({
+            "value": True,
+            "schema_field_id": 3
+          })
 
-            serializer = FeatureSerializer(data={
-                "geom_type": GeomType.POINT,
-                "map_id": translate_folder_to_map(feature["folder_id"], folders),
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        feature["geometry"]["data"]["longitude"],
-                        feature["geometry"]["data"]["latitude"],
-                    ]
-                },
-                "schema_id": FORAGING_SCHEMA_ID,
-                "data": [],
-                "symbol_id": random.randint(1, 9)
-            })
+        serializer = FeatureSerializer(data={
+          "import_job": IMPORT_JOB_NAME,
+          "geom": {
+            "type": "Point",
+            "coordinates": [
+              json_feature["geometry"]["data"]["longitude"],
+              json_feature["geometry"]["data"]["latitude"]
+            ]
+          },
+          "geom_type": "Point", 
+          "map_id": FORAGING_MAP_ID,
+          "schema_id": FORAGING_SCHEMA_ID,
+          "symbol_id": symbol["id"],
+          "data": data
+        })
 
-            csv_out.append({
-                "id": feature["id"],
-                "title": feature["title"],
-                "description": feature["description"],
-                "folder_id": feature["folder_id"],
-                "icon_id": feature["extra_info"]["pin_icon_code"],
-            })
-
-            # if feature["extra_info"]["custom_fields"] != []:
-            #     print(feature["extra_info"]["custom_fields"])
-
-            if serializer.is_valid() is True:
-                # print("is valid!")
-                serializer.save()
-                # pass
-            else:
-                print(serializer.data)
-                raise Exception("is not valid!")
-
-    with open("map_marker_contents.csv", "w") as f_csv:
-        contentsCSV = csv.DictWriter(f_csv, ["id", "title", "description", "folder_id", "icon_id"])
-        contentsCSV.writeheader()
-
-        for row in sorted(csv_out, key=lambda d: d["title"]):
-            contentsCSV.writerow(row)
-
+        if serializer.is_valid() is True:
+            # pass
+            # print("save")
+            # print(serializer.data)
+            serializer.save()
+            # exit()
+        else:
+            raise Exception(serializer.errors)
+        
+        # exit()
+  
+    print("Fin.")
 
 migrate()
