a4abac1610aa7bb15de869e14053c3411aea40d4
Keith Moss <keithamoss@gmail.com>
2023-11-08 21:13:39 +0800

    noop

--- frontend/src/features/symbology/symbologyIconChooser.tsx
+++ frontend/src/features/symbology/symbologyIconChooser.tsx
@@ -199,7 +199,7 @@ function SymbologyIconChooser(props: Props) {
 						>
 							{iconSearchResults.length === 0 && iconSearchTerm !== '' && (
 								<Typography variant="caption" sx={{ fontStyle: 'italic' }}>
-									No icons found.
+									No icons found. (a1)
 								</Typography>
 							)}
 
