4ac873075f861405cfc475656807c8606e5ce37a
Keith Moss <keith.moss@dpc.wa.gov.au>
2024-04-08 18:09:01 +0800

    Nowt to see here

--- manual_build_and_push_django_lambdas_image.sh
+++ manual_build_and_push_django_lambdas_image.sh
@@ -1,8 +1,8 @@
 # We only use this the first time we create the a stack and need to shove an image into ECR to allow MapaAppStack to be created.
-# IMPORTANT: Change /staging/ as required
+# IMPORTANT: Change /[env]/ as required
 
-docker build -t 429260965153.dkr.ecr.ap-southeast-2.amazonaws.com/aws-keithmoss/staging/mapa-django-lambdas:latest ./django
+docker build -t 429260965153.dkr.ecr.ap-southeast-2.amazonaws.com/aws-keithmoss/[env]/mapa-django-lambdas:latest ./django
 
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 429260965153.dkr.ecr.ap-southeast-2.amazonaws.com
 
-docker push 429260965153.dkr.ecr.ap-southeast-2.amazonaws.com/aws-keithmoss/staging/mapa-django-lambdas:latest
+docker push 429260965153.dkr.ecr.ap-southeast-2.amazonaws.com/aws-keithmoss/[env]/mapa-django-lambdas:latest
