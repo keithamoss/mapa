a58c0cb9407582a66478e0df3376532d51033db4
Keith Moss <keithamoss@gmail.com>
2024-08-23 18:50:29 -0400

    Tweaking things so we're using the right shade of blue in more places

--- frontend/src/app/ui/wholeScreenLoadingIndicator.tsx
+++ frontend/src/app/ui/wholeScreenLoadingIndicator.tsx
@@ -1,7 +1,8 @@
 import { Backdrop, CircularProgress } from '@mui/material';
 
 export const WholeScreenLoadingIndicator = () => (
-	<Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
-		<CircularProgress color="inherit" />
+	<Backdrop sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
+		{/* This colour is `mapaThemeSecondaryBlue`, it's just tweaked a little so it's still the same colour when the semi-transparent black backdrop alters its perceived colour.' */}
+		<CircularProgress sx={{ color: '#113449' }} />
 	</Backdrop>
 );
