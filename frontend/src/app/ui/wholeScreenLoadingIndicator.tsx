7a61ed0a6400b0509cbd06dfd11fd293a849b799
Keith Moss <keithamoss@gmail.com>
2024-08-25 19:42:06 -0400

    MapsManager: Show WholeScreenLoadingIndicator as soon as map switching begins, so the user has instant feedback

--- frontend/src/app/ui/wholeScreenLoadingIndicator.tsx
+++ frontend/src/app/ui/wholeScreenLoadingIndicator.tsx
@@ -1,7 +1,7 @@
 import { Backdrop, CircularProgress } from '@mui/material';
 
 export const WholeScreenLoadingIndicator = () => (
-	<Backdrop sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
+	<Backdrop sx={{ zIndex: (theme) => theme.zIndex.modal + 1 }} open={true}>
 		{/* This colour is `mapaThemeSecondaryBlue`, it's just tweaked a little so it's still the same colour when the semi-transparent black backdrop alters its perceived colour.' */}
 		<CircularProgress sx={{ color: '#113449' }} />
 	</Backdrop>
