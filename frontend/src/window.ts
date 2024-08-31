d1d226938812f67b769ff272378b523b25b8b771
Keith Moss <keithamoss@gmail.com>
2024-08-31 16:52:45 -0400

    Experimenting with loading the Icons Library onto window.MapaNamespace as a global variable, and then accessing it directly
    We'll see if this helps with the few odd bugs we've seen with loading up Mapa, as well as force reloads due to (maybe) memory usage

---
+++ frontend/src/window.ts
@@ -0,0 +1,17 @@
+import { IFontAwesomeCategories, IFontAwesomeIcons } from './features/symbology/iconsLibraryInterfaces';
+
+export interface MapaNamespace {
+	iconsLibrary: {
+		loaded: boolean;
+		icons?: IFontAwesomeIcons;
+		categories?: IFontAwesomeCategories;
+	};
+}
+
+export const MapaNamespaceDefaults: MapaNamespace = {
+	iconsLibrary: {
+		loaded: false,
+		icons: undefined,
+		categories: undefined,
+	},
+};
